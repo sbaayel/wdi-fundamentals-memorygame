8b6d7ef34d4e398da8bf4b8ceca640d79a9d807a
Sbaayel <baayel@gmail.com>
2020-02-03 16:43:24 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,18 +2,25 @@ console.log("Up and running");
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [ ];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardsInPlay);
-let cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardsInPlay[1]);
-if (i = 0, cardsInPlay.length === 2, ++i) {
-	console.log(cardsInPlay.length);
 
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
 	} else{
 		alert('Sorry try again');
-	};
-};
+	}
+}
+
+function flipCard(cardId){
+
+if (i = 0, cardsInPlay.length === 2, ++i) {
+	console.log(cardsInPlay.length);
+
+checkForMatch();		
+}
+console.log("User flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+}
+flipCard (0);
+flipCard (2);
